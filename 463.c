5a81a35c3092753cbe364a4b626c86e513118254
Brenda Neves <168302561+Adnerb7@users.noreply.github.com>
2025-10-23 22:05:50 -0300

    Add files via upload

---
+++ 463.c
@@ -0,0 +1,18 @@
+//Exercício 4.6.2 do Feofiloff que pede uma função para remover de uma lista encadeada todos os elementos que contém x 
+
+void RemoveX(int x, celula *lst) {
+    celula *p, *q;
+    p = lst;
+    q = lst->seg;
+
+    while (q != NULL) {
+        if (q->conteudo == x) {
+            p->seg = q->seg;
+            free(q);
+            q = p->seg;
+        } else {
+            p = q;
+            q = q->seg;
+        }
+    }
+}

bd5bc991e28f42a3e03a0970c804e4a380288524
Brenda Neves <168302561+Adnerb7@users.noreply.github.com>
2025-10-28 22:03:39 -0300

    Add files via upload

---
+++ 542.c
@@ -0,0 +1,45 @@
+// Implementação de uma fila circular usando lista ligada em C. Exercício 5.4.2 da apostila do Feofiloff 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct cel {
+    int dado;
+    struct cel *prox;
+} Celula;
+
+Celula* cria_fila() {
+    Celula *cabeca = (Celula*) malloc(sizeof(Celula));
+    cabeca->prox = cabeca;
+    return cabeca;
+}
+void enfileira(Celula *cabeca, int x) {
+    Celula *novo = (Celula*) malloc(sizeof(Celula));
+    novo->dado = x;
+
+    Celula *p = cabeca;
+    while (p->prox != cabeca) p = p->prox;
+    p->prox = novo;
+    novo->prox = cabeca;
+}
+
+int desenfileira(Celula *cabeca) {
+    if (cabeca->prox == cabeca) {
+        printf("Fila vazia!\n");
+        return -1;
+    }
+    Celula *remover = cabeca->prox;
+    int x = remover->dado;
+    cabeca->prox = remover->prox;
+    free(remover);
+    return x;
+}
+void mostra_fila(Celula *cabeca) {
+    Celula *p = cabeca->prox;
+    while (p != cabeca) {
+        printf("%d ", p->dado);
+        p = p->prox;
+    }
+    printf("\n");
+
+}
